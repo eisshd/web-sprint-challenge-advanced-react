e56ed40fa0b5450f79406222313ffaf5f15d2574
Harley Eiss <ice.eiss@gmail.com>
2023-06-30 22:18:20 -0400

    added tests

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,12 @@
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react'
+import App from './App';
+import AppFunctional from './frontend/components/AppFunctional'
+import AppClass from './frontend/components/AppClass'
+
+
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+test("Renders without errors", ()=> {
+  render(<App/>);
+});
+
