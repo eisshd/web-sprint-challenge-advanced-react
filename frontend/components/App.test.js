a9435f960b6bb0ac28ea2a865c32a97e7aca856d
Harley Eiss <ice.eiss@gmail.com>
2023-06-30 22:38:15 -0400

    commit

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react'
-import App from './App';
 import AppFunctional from './frontend/components/AppFunctional'
 import AppClass from './frontend/components/AppClass'
 
 
 // Write your tests here
 test("Renders without errors", ()=> {
-  render(<App/>);
+  render(<AppClass/>);
+  render(<AppFunctional/>)
+
+  
 });
+
+
 
